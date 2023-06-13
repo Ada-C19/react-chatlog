a8b6ea32d2a07accfba9612017d7cfbdcc012fa2
Ana Silvera <anasilverx@gmail.com>
2023-06-13 11:36:05 -0700

    initial commit. added console log for chatMessahes to insepct contents of array

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  console.log(chatMessages)
+  console.log('Hello World')
+  console.log(chatMessages[0])
+  console.log(chatMessages[0].id)
+  console.log(chatMessages[0].sender)
+  console.log(chatMessages[0].body)
+  console.log(chatMessages[0].timeStamp)
+
   return (
     <div id="App">
       <header>
