31155a66bd58e8981c6f58ef267a29fffa53d247
Julie Loh <jlohusky@gmail.com>
2023-06-13 10:53:22 -0700

    chatEntry components chatEntry props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatData = [
+    {
+      id: 1,
+      sender:'Vladimir',
+      body:'why are you arguing with me',
+      timeStamp:'2018-05-29T22:49:06+00:00',
+      liked: false,
+    }
+  ];
+
+  const chatEntryComponents = chatData.map(chatMsg => {
+    return (
+      <ul>
+        <ChatEntry name={chatMsg.sender} body={chatMsg.body} timeStamp={chatMsg.timeStamp}></ChatEntry>
+      </ul>
+
+    )
+  });
+
   return (
     <div id="App">
       <header>
@@ -11,6 +31,9 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ul>
+          {chatEntryComponents}
+        </ul>
       </main>
     </div>
   );
