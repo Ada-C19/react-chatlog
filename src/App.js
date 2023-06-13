4e739e4f9d58873ce01a03de6f1d0f3350f49788
Anna Lima <annavanwesep@gmail.com>
2023-06-13 11:20:16 -0700

    updated chatentry to take props and display contents based on data passed to props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const props = {
+      "id": 1,
+      "sender":"Vladimir",
+      "body":"why are you arguing with me",
+      "timeStamp":"2018-05-29T22:49:06+00:00",
+      "liked": false
+    }
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
+        <ChatEntry props={props} />     
+      {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
 };
-
 export default App;
