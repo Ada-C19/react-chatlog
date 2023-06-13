42fba4ccc89ded409d4a89f7f7675529ebf2c2a5
chickenandegg <tei.villanueva@gmail.com>
2023-06-13 11:20:32 -0700

    Tested one chat entry block to show the sender and body

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+
+  const testMessage = chatMessages[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry sender = {testMessage.sender} body = {testMessage.body}></ChatEntry>
         <ChatEntry></ChatEntry>
+
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
-import data from '../data/messages';
+import data from '../data/messages.json';
 
 const ChatEntry = (props) => {
+
+  const { sender, body } = props;
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">hello</h2>
+      <h2 className="entry-name">{ sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
+        <p>{body}</p>
         <p className="entry-time">Replace with TimeStamp component</p>
         <button className="like">🤍</button>
       </section>
