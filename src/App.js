7fae5d9098b3dad8426304323f900ed720d3a17b
dreinicke27 <danicareinicke@gmail.com>
2023-06-13 11:19:55 -0700

    Changed header to use the names of the two senders

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
-import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
-  const oneChat = chatMessages[0];
-  console.log(oneChat)
+  // const oneChat = chatMessages[0];
+  // console.log(oneChat)
 
   return (
     <div id="App">
       <header>
-        <h1>Chat Log</h1>
+        <h1>Chat between {chatMessages[0].sender} and {chatMessages[1].sender}</h1>
       </header>
       <main>
         {/*<ChatEntry name={oneChat.sender} body={oneChat.body} time={oneChat.timeStamp}></ChatEntry>*/}
