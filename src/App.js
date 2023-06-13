52c5d5ad293078caf0661277239589b951995ddf
Ericka <erickamoreno529@gmail.com>
2023-06-13 11:19:56 -0700

    chat entry to intake props and show message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry'
 
 const App = () => {
+  const message  = {
+    "id": 1,
+    "sender":"Vladimir",
+    "body":"why are you arguing with me",
+    "timeStamp":"2018-05-29T22:49:06+00:00",
+    "liked": false,
+  };
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        
+        {/* Wave 1: render one chat entry component */}
+        <ChatEntry props={message}/>
+        {/* <ChatLog props={log}/> */}
+        {/* {Wave 02: Render ChatLog component */}
+        
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import {DateTime} from 'luxon';
 
-const ChatEntry = (props) => {
+const ChatEntry = ({props}) => {
+  console.log(props)
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time">{props.timeStamp}</p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +18,12 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  "id": Number.isRequired,
+  "sender": String.isRequired,
+  "body": String.isRequired, 
+  "timeStamp": DateTime.isRequired,
+  "liked": Boolean.isRequired,
 };
+
 
 export default ChatEntry;
