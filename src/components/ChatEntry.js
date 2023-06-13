4785b0ded63dd3bea4158f8a2a768d0c2c08725e
dreinicke27 <danicareinicke@gmail.com>
2023-06-13 12:59:23 -0700

    updated chatEntry so that like button toggles from red to white

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,33 +2,28 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
+import { useState } from 'react';
 
 const ChatEntry = (props) => {
   const id = props.id;
+  const [isLiked, setIsLiked] = useState(props.liked);
+  const swapLiked = () => {
+    setIsLiked(!isLiked);
+  };
+  const buttonContent = isLiked ? '❤️' : '🤍';
 
-  if (id % 2 !== 0) {
-    return(
-      <div className="chat-entry local">
-      <h2 className="entry-name">{props.sender}</h2>
-      <section className="entry-bubble">
-        <p>{props.body}</p>
-        <p className="entry-time"><TimeStamp time={props.timeStamp}></TimeStamp></p>
-        <button className="like">🤍</button>
-      </section>
-    </div>
-      );
-  } else {
-    return(
-      <div className="chat-entry remote">
-      <h2 className="entry-name">{props.sender}</h2>
-      <section className="entry-bubble">
-        <p>{props.body}</p>
-        <p className="entry-time"><TimeStamp time={props.timeStamp}></TimeStamp></p>
-        <button className="like">🤍</button>
-      </section>
-    </div>
-      );
-  }
+  const location = (id % 2 !== 0) ? 'chat-entry local' : 'chat-entry remote';
+
+  return(
+    <div className={location}>
+    <h2 className="entry-name">{props.sender}</h2>
+    <section className="entry-bubble">
+      <p>{props.body}</p>
+      <p className="entry-time"><TimeStamp time={props.timeStamp}></TimeStamp></p>
+      <button onClick={swapLiked}>{buttonContent}</button>
+    </section>
+  </div>
+  );
 };
 
 ChatEntry.propTypes = {
