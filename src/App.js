003974821fb7250b2693d42af51b7c15c69ef980
April Zhang <aprilzh23@gmail.com>
2023-06-14 14:03:23 -0700

    display a single chat message bubble

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
+
+const oneData = {
+  id: 1,
+  sender:'Vladimir',
+  body:'why are you arguing with me',
+  timeStamp:'2018-05-29T22:49:06+00:00',
+  liked: false
+}
 
 const App = () => {
   return (
@@ -9,8 +18,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry senderName={oneData.sender} messageBody={oneData.body}></ChatEntry>
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 
+
+
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{ props.senderName }</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
+        <p>{ props.messageBody }</p>
         <p className="entry-time">Replace with TimeStamp component</p>
         <button className="like">🤍</button>
       </section>
@@ -17,6 +19,10 @@ const ChatEntry = (props) => {
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  // id: PropTypes.number,
+  senderName: PropTypes.string,
+  messageBody: PropTypes.string
+  // liked: PropTypes.bool
 };
 
 export default ChatEntry;
