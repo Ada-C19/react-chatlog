b5b2f3b1af6713007e084a17a5c7cc9aaccd7e1c
Sophia Tran <sophiat8832@gmail.com>
2023-06-14 14:22:00 -0700

    created exampleData and rendered ChatEntry component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
+const exampleData = {
+  id: 1,
+  sender: 'Vladimir',
+  body: 'why are you arguing with me',
+  timeStamp: '2018-05-29T22:49:06+00:00',
+  liked: false
+};
 
 const App = () => {
   return (
@@ -11,6 +20,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry 
+          sender={exampleData.sender} 
+          body={exampleData.body} 
+          timeStamp={exampleData.timeStamp}>
+        </ChatEntry>
       </main>
     </div>
   );
