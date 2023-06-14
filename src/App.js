72fd97a3d12cbe260ad8c5ab24f5ee74414f9113
Rediet Medhane <redietkmedhane@gmail.com>
2023-06-14 14:41:26 -0700

    set up props for one text message, displays correctly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const testData = {
+        id:7,
+        sender:'Joe Biden',
+        body:"Get out by 8am.  I'll count the silverware",
+        timeStamp:'2018-05-18T22:12:03Z',
+        liked:false
+  }    
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estagon</h1>
       </header>
       <main>
+        <ChatEntry sender={testData.sender} body={testData.body} timeStamp={testData.timeStamp}></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
