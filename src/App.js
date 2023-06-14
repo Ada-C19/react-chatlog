2ce5ec3375a4d868a8f9734c5d6ee585205ce0f2
Ana Silvera <anasilverx@gmail.com>
2023-06-14 13:45:15 -0700

    replaced individual chatEntry component for chatLog component that contains ChatEntry. assigned chatMessages as objected passed to chatlog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
   const chatMessage1 = chatMessages[0]
@@ -15,8 +16,10 @@ const App = () => {
         <h1>Chat Between Vladimir and Estragon</h1>
       </header>
       <main>
-        <ChatEntry sender={chatMessage1.sender} body={chatMessage1.body} timeStamp={chatMessage1.timeStamp}></ChatEntry>
-        <ChatEntry sender={chatMessage2.sender} body={chatMessage2.body} timeStamp={chatMessage2.timeStamp}></ChatEntry>
+        <ChatLog chatMessages={ chatMessages }></ChatLog>
+        
+        {/* <ChatEntry sender={chatMessage1.sender} body={chatMessage1.body} timeStamp={chatMessage1.timeStamp}></ChatEntry>
+        <ChatEntry sender={chatMessage2.sender} body={chatMessage2.body} timeStamp={chatMessage2.timeStamp}></ChatEntry> */}
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
