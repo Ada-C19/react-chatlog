b714326eb16cbc495313628007471e81b2fc57f3
Monica <monicalagdaan@hotmail.com>
2023-06-14 13:49:49 -0700

    fixed key bug

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import { DateTime } from 'luxon';
+import TimeStamp from './TimeStamp';
 
-const ChatEntry = ({props}) => {
+const ChatEntry = (props) => {
+
 
   return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">{props.sender}</h2>
-      <section className="entry-bubble">
+    <div className='chat-entry local'>
+      <h2 className='entry-name'>{props.sender}</h2>
+      <section className='entry-bubble'>
         <p>{props.body}</p>
-        <p className="entry-time">{props.timeStamp}</p>
-        <button className="like">🤍</button>
+          <p className='entry-time'>
+              <TimeStamp time={props.timeStamp} /></p>
+        <button className='like'>🤍</button>
       </section>
     </div>
   );
 };
 
 ChatEntry.propTypes = {
-  // "id": Number.isRequired,
-  // "sender": String.isRequired, 
-  // "body": String.isRequired, 
-  // "timeStamp": DateTime.isRequired,
-  // "liked": Boolean.isRequired,
+  'id': PropTypes.number,
+  'sender': PropTypes.string.isRequired, 
+  'body': PropTypes.string.isRequired, 
+  'timeStamp': PropTypes.string.isRequired,
+  'liked': PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
