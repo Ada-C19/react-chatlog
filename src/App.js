5f7a0575ab849bca31b5a5f1a8aea607a45046b5
jasmine <jtmsoto@gmail.com>
2023-06-14 22:53:29 -0700

    Remove ChatEntry import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 import { useState } from 'react';
 
