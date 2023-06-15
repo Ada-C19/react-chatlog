51a5980e1b2b821352ffbb9d198b67fdf213e868
Ana Villalobos <ana.villalobos309@gmail.com>
2023-06-14 20:33:25 -0700

    Renders one chat message bubble and passes associated Tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Between Vladimir and Estrogen</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry 
+          sender = "Vladimir"
+          body = "why are you arguing with me"
+          timeStamp = "2018-05-29T22:49:06+00:00"
+        />
       </main>
     </div>
   );
