09e46b9c989f0675b5e9f45797733ad5210adaf4
Lyuda Kim <lyudarkim@gmail.com>
2023-06-14 20:12:13 -0700

    Add behavior to heart button in ChatEntry.js

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,16 +1,38 @@
-import React from 'react';
-import './ChatEntry.css';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './ChatEntry.css';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const updateChatLog = () => {
+    props.onUpdateMessage({
+      id: props.id, 
+      sender: props.sender,
+      body: props.body,
+      timeStamp: props.timeStamp,
+      liked: !isLiked,
+    });
+
+    // Change the state of isLiked
+    setIsLiked(!isLiked);
+  };
+  
+  // Change heart emoji from white to red if message is liked
+  const heart = isLiked ? '❤️' : '🤍';
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">{props.sender}</h2>
+      <h2 className="entry-name">
+        { props.sender }
+      </h2>
       <section className="entry-bubble">
-        <p>{props.body}</p>
-        <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
-        <button className="like">🤍</button>
+        <p>{ props.body }</p>
+        <p className="entry-time">
+          <TimeStamp time={ props.timeStamp }/>
+        </p>
+        <button onClick={ updateChatLog }>{ heart }</button>
       </section>
     </div>
   );
@@ -19,7 +41,8 @@ const ChatEntry = (props) => {
 ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  timeStamp: PropTypes.string.isRequired
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
