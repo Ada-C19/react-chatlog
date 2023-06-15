2ed13fa1b16132022ab746b10b19bed6314b5eaf
Sophia Tran <sophiat8832@gmail.com>
2023-06-15 16:01:46 -0700

    used useState to toggle and render like button feature

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+
+  // const [likesCount, setLikesCount] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLikeButton = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
@@ -12,7 +21,7 @@ const ChatEntry = (props) => {
         <p className="entry-time">
           <TimeStamp time={props.timeStamp}></TimeStamp>
         </p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={toggleLikeButton}>{isLiked ? '❤️' : '🤍'}</button>
       </section>
     </div>
   );
