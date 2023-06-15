06ef8a692ea076b85eeb9e92f56e1b7aa439af8c
Jay King <jayriverking@gmail.com>
2023-06-15 14:36:44 -0400

    first element works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <ChatEntry></ChatEntry>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import chatMessages from '../data/messages.json'
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  const firstElement = chatMessages[0];
+  console.log(firstElement);
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{firstElement.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{firstElement.body}</p>
+        <p className="entry-time"><TimeStamp time={firstElement.timeStamp} /></p>
         <button className="like">🤍</button>
       </section>
     </div>
