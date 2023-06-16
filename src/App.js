bba13678fb147712979fedac4f719f9ea119ca7c
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-06-16 06:26:29 -0600

    Wave 1 looks good.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import ChatEntry from './components/ChatEntry';
 import chatMessages from './data/messages.json';
 
 const App = () => {
+  const firstChatMessage= chatMessages[0];
+  console.log(firstChatMessage);
+  
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>🤖 When Robots Collide 🤖 </h1>
       </header>
       <main>
+        <ChatEntry
+          sender={firstChatMessage.sender}
+          body={firstChatMessage.body}
+          timeStamp={firstChatMessage.timeStamp}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
