7c57ca1bc3304caf2a407919f7f385cd46c8bb1c
Virginia Ramos <virginia.rare@gmail.com>
2023-06-16 01:01:27 -0500

    Wave 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const firstMessage = chatMessages[0];
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstMessage.sender}
+          body={firstMessage.body}
+          timeStamp={firstMessage.timeStamp}
+        />
       </main>
     </div>
   );
 };
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <div id="App">
+//       <header>
+//         <h1>Application title</h1>
+//       </header>
+//       <main>
+//         {/* Wave 01: Render one ChatEntry component
+//         Wave 02: Render ChatLog component */}
+//       </main>
+//     </div>
+//   );
+// };
+
+// export default App;
