3e272b2115171223c6b7f9f59a6f00cd091c4318
areeg <127351347+arejib@users.noreply.github.com>
2023-06-15 19:28:29 -0500

    Completed Wave 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const renderChatEntries = () => {
+    return chatMessages.map((message) => {
+      return <ChatEntry sender={message.sender} body={message.body} timeStamp={message.timeStamp} key={message.id} />
+    });
+  };
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <div>
+          {renderChatEntries()}
+        </div>
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
