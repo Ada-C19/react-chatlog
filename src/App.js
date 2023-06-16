df21cd89ab5f3694a4b81a1eb146bfd987c91ec0
Airis Wang <airis.wang2014@fgvmail.br>
2023-06-15 21:52:38 -0400

    finished wave 01

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+import TimeStamp from './components/TimeStamp';
 
 const App = () => {
   return (
@@ -9,6 +11,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry sender={chatMessages[0]['sender']} body={chatMessages[0]['body']} timeStamp={chatMessages[0]['timeStamp']} />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
