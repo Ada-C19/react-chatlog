cc27091f27bbdc5d11f584b74a736f710dbe6baf
fabiola gonzalez <gonzalezfabs@gmail.com>
2023-06-17 15:24:36 -0700

    wave 1 passes all tests, no single message text shown

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+import TimeStamp from './components/TimeStamp';
 
 const App = () => {
+  console.log(chatMessages)
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry></ChatEntry>
+      
+
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  // const chatData = [
+  //   {
+  //     id: 1,
+  //     sender:'Vladimir',
+  //     body:'why are you arguing with me',
+  //     timeStamp:'2018-05-29T22:49:06+00:00',
+  //     liked: false
+  //   }
+  // ]
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.body}</p>
+        <p className="entry-time"><TimeStamp time={props.timeStamp} /></p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +26,11 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  // id: PropTypes.number.isRequired,
+  sender: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+  timeStamp: PropTypes.string.isRequired,
+  // liked: PropTypes.bool.isRequired
 };
 
 export default ChatEntry;
