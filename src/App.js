78eef130dc4874ccddc96b00a22270a867010dec
Ariel Williams <arielwilliams121013@gmail.com>
2023-06-17 15:21:15 -0700

    Wrote renderChatLog() function in App.js. Called renderChatLog() inside the main JSX tag in App.js. Rendering all chat messages on the DOM.

--- src/App.js
+++ src/App.js
@@ -4,19 +4,34 @@ import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
 
 
-const sender = chatMessages[0].sender
-const body = chatMessages[0].body
-const timeStamp = chatMessages[0].timeStamp
+// const sender = chatMessages[0].sender
+// const body = chatMessages[0].body
+// const timeStamp = chatMessages[0].timeStamp
 
 
 const App = () => {
+  // renderChatLog returns a new array of msgs in JSX
+  const renderChatLog = () => {
+    return (
+      chatMessages.map((message) => {
+        return (
+          <ChatEntry 
+            sender = { message.sender }
+            body = { message.body }
+            timeStamp= { message.timeStamp }
+          />
+        )
+      })
+    )
+  }
+
   return (
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        <ChatEntry sender={sender} body={body} timeStamp={timeStamp}></ChatEntry>
+        {renderChatLog()}
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
