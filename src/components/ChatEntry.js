90a82f0232d4aa14fcc46a3000bd0e260980f9e8
Sophia Tran <sophiat8832@gmail.com>
2023-06-17 16:22:59 -0700

    added proptypes for id, liked, updateLiked

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -30,9 +30,12 @@ const ChatEntry = (props) => {
 
 ChatEntry.propTypes = {
   //Fill with correct proptypes
+  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
-  timeStamp: PropTypes.string.isRequired
+  timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  updateLiked: PropTypes.func
 };
 
 export default ChatEntry;
