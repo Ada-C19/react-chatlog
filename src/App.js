49e83992e761eecdad53fc74fa48ea407aa5cc0f
Sophia Tran <sophiat8832@gmail.com>
2023-06-17 14:48:25 -0700

    created state for chatMessages; implemented updateLiked

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
+
+  const [messages, setMessages] = useState(chatMessages);
+
+  const updateLiked = (messageId) => {
+    const updatedMessages = messages.map(message => {
+      if (messageId === message.id) {
+        message.liked = !message.liked;
+      }
+    });
+    setMessages(updatedMessages);
+  };
+
   return (
     <div id="App">
       <header>
