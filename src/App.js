3fdbf5ed1e80b721b249a93d7ee32678fb090a93
La Tasha Pollard <latashajpollard@gmail.com>
2023-06-17 00:05:15 -0500

    updates ChatEntry and App components to complete wave 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatData = chatMessages;
+  // console.log(chatMessages);
   return (
     <div id="App">
       <header>
@@ -11,6 +14,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={chatData[0].sender}
+          body={chatData[0].body}
+          timeStamp={chatData[0].timeStamp}
+        />
       </main>
     </div>
   );
