b1cb021f84dd47eb16620a60f71c79dba64d4a17
Sophia Tran <sophiat8832@gmail.com>
2023-06-17 16:11:01 -0700

    created element for like count; implemented likeCount function

--- src/App.js
+++ src/App.js
@@ -12,15 +12,28 @@ const App = () => {
       if (messageId === message.id) {
         message.liked = !message.liked;
       }
-      return {...messages}
+      return {...message}
     });
     setMessages(updatedMessages);
+  };
+
+  const likeCount = () => {
+    let likeCount = 0;
+    for (const message of messages) {
+      if (message.liked) {
+        likeCount++;
+      }
+    }
+    return likeCount;
   };
 
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
+        <section> 
+          <span id="heartWidget" className="widget">{ () => likeCount() } ❤️'s liked</span>
+        </section>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
