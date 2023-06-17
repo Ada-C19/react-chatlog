2c83230196172c0e749c8931f7d9d5712c84fe64
Ariel Williams <arielwilliams121013@gmail.com>
2023-06-17 14:04:42 -0700

    Worked on App.js and ChatEntry.js files. Currently displaying the first message from the messages.json file with the info sender, body, and timeStamp.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+
+
+const sender = chatMessages[0].sender
+const body = chatMessages[0].body
+const timeStamp = chatMessages[0].timeStamp
+
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
+        <ChatEntry sender={sender} body={body} timeStamp={timeStamp}></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
