7a13f576bca846c89f0472479d7a3f26a8ec340d
Tigist Woldearegai <tigistlina@gmail.com>
2023-06-18 02:22:00 -0500

    creats ChatLog component and updates the App componenet to display an entire chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry'
+import ChatLog from './components/ChatLog';
 
 const App = () => {
   console.log(chatMessages);
-  const {sender, body, timeStamp} = chatMessages[0];
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {<ChatEntry
-          sender={sender}
-          body={body}
-          timeStamp={timeStamp}
-          />
-
-          /* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog entries={chatMessages}/>
       </main>
     </div>
   );
