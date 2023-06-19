692e493cdb38705ed0a2068a79560049c6d55ec1
halimAremu <halimat3000@gmail.com>
2023-06-19 15:44:16 -0500

    completed all waves

--- src/App.js
+++ src/App.js
@@ -19,15 +19,23 @@ const App = () => {
     setTextMessage(isliked);
   }
 
+  const totalLikes = (data)=> {
+    const newList = data.filter((ele)=>ele.liked===true)
+    return newList.length
+  }
+
+  const likedCount = totalLikes(textMessage)  
   return (
     <div id="App">
       <header>
         <h1>Chat App</h1>
       </header>
       <main>
+        <h1>{likedCount} ❤️s</h1>
         {/* <ChatEntry id = 1 timeStamp = "2018-05-29T22:49:06+00:00" body = "why are you arguing with me" 
         sender = "Vladimir" liked = false/> */}
         <ChatLog entries={textMessage} likedFunc = {likedMessage}/>
+        <h3>Liked Messages: {likedCount}</h3>
       </main>
     </div>
   );
