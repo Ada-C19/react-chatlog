c8da9d72960a32a4f8402006a8c3639c5db9a5f6
Ariel Higuera <purpl3m00ns@gmail.com>
2023-06-20 10:38:28 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [chatData, setChatData] = useState(chatMessages);
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  const updateChatEntry = (updatedMessage) => {
+    const messages = chatData.map((message) => {
+      if (message.id === updatedMessage.id) {
+        if (updatedMessage.liked) {
+          setLikeCount(likeCount +1)
+        } else {
+          setLikeCount(likeCount - 1)
+        }
+
+        return updatedMessage;
+      }
+      return message;
+    })
+    setChatData(messages);
+  };
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat Log</h1>
+        <section class="likeCount">
+          {likeCount} ❤️s
+        </section>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatLog entries={chatData} onToggleLike={updateChatEntry}/>
       </main>
     </div>
   );
