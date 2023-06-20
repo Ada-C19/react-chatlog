5a97298680e0cc89523cbc3c6934466ddd3062bf
ystartc <startceva.dev@gmail.com>
2023-06-19 21:34:14 -0700

    Created State in App container, passes props from state to ChatEntry

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry/ChatEntry';
 import './App.css';
 
 const App = () => {
+  const [chatEntries, setChatEntries] = useState(chatMessages);
   return (
     <section id='App'>
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
+        <ChatEntry chatEntries={chatEntries} />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
