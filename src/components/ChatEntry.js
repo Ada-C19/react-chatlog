3883bd2873d54850eefed5f42ebb50299494d9aa
Agnes Kim <agneskim4@gmail.com>
2023-06-20 13:58:23 -0400

    create updateLike to toggle singleEntry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -5,6 +5,17 @@ import TimeStamp from './TimeStamp'
 
 const ChatEntry = (singleEntry) => {
   // const singleEntry = props.props
+  const onLikeButtonClicked = () => {
+      const updateLike = {
+        key: singleEntry.key,
+        sender: singleEntry.sender,
+        body: singleEntry.body,
+        liked: !singleEntry.liked
+      }
+      singleEntry.onUpdate(updateLike);
+  };
+  const heart = singleEntry.liked ? '💜' : '🤍'; 
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{singleEntry.sender}</h2>
@@ -12,7 +23,7 @@ const ChatEntry = (singleEntry) => {
         <p>{singleEntry.body}</p>
         <TimeStamp time = {singleEntry.time}> </TimeStamp>
         {/* <p className="entry-time">{singleEntry.timeStamp}</p> */}
-        <button className="like">🤍</button>
+        <button className='liked' onClick={onLikeButtonClicked}>{heart}</button>
       </section>
     </div>
   );
