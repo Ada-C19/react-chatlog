68fd2072bcd003221e5d85ab33dff48c807c8b70
Aiyana Ball <ballaiyana@gmail.com>
2023-06-19 19:50:24 -0700

    Imported chatEntry, changed app title, added first chatEntry in App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry.js';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry 
+            sender={chatMessages[0].sender}
+            body={chatMessages[0].body}
+            timeStamp={chatMessages[0].timeStamp}
+          />
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
