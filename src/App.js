7ce584eedfcf58e2d7a7bc454eb13d00c7908453
Elizabeth Zachary <elizabethzachary96@gmail.com>
2023-06-20 14:19:26 -0700

    wave 1 presentational component completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+
+  const testChat = chatMessages[0];
+
+  console.log(chatMessages)
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry
+          id={testChat.id}
+          liked={testChat.liked}
+          sender={testChat.sender}
+          body={testChat.body}
+          timeStamp={testChat.timeStamp}
+        />
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
