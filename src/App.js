5d74691cbfeab33badd27c9f1e0b7f649f718728
Sophia Tran <sophiat8832@gmail.com>
2023-06-20 14:05:15 -0700

    fixed updateLiked and removed state from ChatEntry

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
+// import ColorChoice from './components/ColorChoice';
 
 const App = () => {
 
   const [messages, setMessages] = useState(chatMessages);
 
+  // const [colorData, setColorData] = useState('');
+
   const updateLiked = (messageId) => {
     const updatedMessages = messages.map(message => {
       if (messageId === message.id) {
-        message.liked = !message.liked;
+        return {
+          ...message,
+          liked: !message.liked
+        }
+        
       }
-      return {...message}
+
+      return message;
     });
     setMessages(updatedMessages);
   };
@@ -27,18 +35,26 @@ const App = () => {
     return likeCount;
   };
 
+  // const setColorCallback = (color) => {
+  //   setColorData(color);
+  //   return color;
+  // };
+
   return (
     <div id="App">
       <header>
         <h1>{`Chat Between ${chatMessages[0].sender} and ${chatMessages[1].sender}`}</h1>
         <section> 
-          <span id="heartWidget" className="widget"> { `${likeCount()} ❤️s liked`}</span>
+          {/* <span>{`${chatMessages[0].sender}'s color:`}</span>
+          <ColorChoice setColorCallback={setColorCallback}></ColorChoice> */}
+          <span id="heartWidget" className="widget">{`${likeCount()} ❤️s liked`}</span>
+          {/* <ColorChoice setColorCallback={setColorCallback}></ColorChoice> */}
         </section>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
-        <ChatLog entries={chatMessages} updateLiked={updateLiked}></ChatLog>
+        <ChatLog entries={messages} updateLiked={updateLiked} /* textColor={colorData} */></ChatLog>
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
 
-  const [isLiked, setIsLiked] = useState(props.liked);
-
   const toggleLikeButton = () => {
     props.updateLiked(props.id);
-    setIsLiked(!isLiked);
   };
 
   return (
     <div className={ props.id % 2 ? 'chat-entry local' : 'chat-entry remote' }>
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
-        <p>{props.body}</p>
+        <p /*className={props.textColor}*/>{props.body}</p>
         <p className="entry-time">
           <TimeStamp time={props.timeStamp}></TimeStamp>
         </p>
-        <button className="like" onClick={toggleLikeButton}>{isLiked ? '❤️' : '🤍'}</button>
+        <button className="like" onClick={toggleLikeButton}>{props.liked ? '❤️' : '🤍'}</button>
       </section>
     </div>
   );
