cb37683cc71e3cbb294459b6fa4f3d87fba80fe4
Sarah Kim <kimsuyon2013@gmail.com>
2023-06-20 15:04:29 -0700

    added proptypes

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,7 +3,7 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
-const ChatEntry = ({sender, body, timeStamp}) => {
+const ChatEntry = ({ id, sender, body, timeStamp}) => {
   return ( 
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
@@ -17,7 +17,10 @@ const ChatEntry = ({sender, body, timeStamp}) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  id:PropTypes.number.isRequired,
+  sender:PropTypes.string.isRequired,
+  body:PropTypes.string.isRequired,
+  timeStamp:PropTypes.string.isRequired
 };
 
 export default ChatEntry;
