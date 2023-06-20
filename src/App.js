8c420213c5a2cd1b3e5cc741165db6523a47edbe
nadia <reach.nadia.saria@gmail.com>
2023-06-20 15:08:37 -0700

    finish wave 01 and start wave 02

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
+  const [messages, setMessages] = useState(chatMessages)
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Nadia's ChatLog</h1>
       </header>
       <main>
+        {/* <ChatEntry props={chatMessages[0]}/> */}
+        <ChatLog entries={messages}/>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
