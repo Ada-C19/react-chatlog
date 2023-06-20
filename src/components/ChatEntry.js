0a423c85de86e413eebd6e1f6cdfd68cc5973390
selene castillo <secast@live.com>
2023-06-20 15:25:14 -0700

    updated heart button toggle

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -3,8 +3,17 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import { DateTime } from 'luxon';
 import TimeStamp from './TimeStamp';
+import { useState } from 'react';
 
 const ChatEntry = (props) => {
+
+  const [isLiked, setIsLiked] = useState(props.isLiked);
+  const heartButton = isLiked ? '❤️':'🤍'
+
+  const toggleLike = () => {
+      setIsLiked(!isLiked);
+      props.updateLike(props.id);
+  }
 
 
   return (
@@ -14,7 +23,7 @@ const ChatEntry = (props) => {
         <p>{props.body}</p>
           <p className="entry-time">
               <TimeStamp time={props.timeStamp} /></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={toggleLike}>{heartButton}</button>
       </section>
     </div>
   );
@@ -26,6 +35,8 @@ ChatEntry.propTypes = {
   "body": PropTypes.string.isRequired, 
   "timeStamp": PropTypes.string.isRequired,
   "liked": PropTypes.bool.isRequired,
+  isLiked: PropTypes.bool,
+  updateLike: PropTypes.func,
 };
 
 export default ChatEntry;
