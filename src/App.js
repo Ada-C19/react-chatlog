54a12c0e5ea8af3b8ee16f0c5d32dbd65dbde5ae
ystartc <startceva.dev@gmail.com>
2023-06-20 00:41:01 -0700

    Created a state for keeping and updating likes count, added to JSX to display it

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ import './App.css';
 
 const App = () => {
   const [chatLogs, setChatLogs] = useState(chatMessages);
+  const [likesCount, setlikesCount] = useState(0);
+
+  const updateLike = logToUpdate => {
+    const updatedLogs = chatLogs.map(log => {
+      if (log.id === logToUpdate.id) {
+        logToUpdate.liked
+          ? setlikesCount(likesCount + 1)
+          : setlikesCount(likesCount - 1);
+        return logToUpdate;
+      }
+      return log;
+    });
+    setChatLogs(updatedLogs);
+  };
+
+  const updateLikesCount = currentCount => setlikesCount();
+
+  const names = [];
   return (
     <section id='App'>
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
+        {likesCount ? <p>{likesCount} ❤️s</p> : <></>}
       </header>
       <main>
-        <ChatLog entries={chatLogs} />
+        <ChatLog
+          entries={chatLogs}
+          updateLike={updateLike}
+          updateLikesCount={updateLikesCount}
+        />
       </main>
     </section>
   );
