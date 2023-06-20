ea938f90deebb8b739dd55ded08bb2d555ddfaf8
Madison Jackson <madi.jackson29@gmail.com>
2023-06-20 14:53:47 -0700

    started tinkering with chatEntry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>ChatBug</h1>
       </header>
       <main>
+        <div><ChatEntry></ChatEntry></div>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
