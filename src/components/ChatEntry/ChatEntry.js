264bce38ee0c3b68f739e50eeab03caf355a853f
ystartc <startceva.dev@gmail.com>
2023-06-19 21:45:19 -0700

    Passed props into ChatEntry and mapped throw each of it to receive it as individual ChatLog

--- src/components/ChatEntry/ChatEntry.js
+++ src/components/ChatEntry/ChatEntry.js
@@ -4,15 +4,27 @@ import ChatLog from './ChatLog/ChatLog';
 import '../ChatEntry.css';
 
 const ChatEntry = props => {
+  const getChatLogJSX = chats => {
+    return chats.map(chat => (
+      <ChatLog
+        sender={chat.sender}
+        body={chat.body}
+        timeStamp={chat.timeStamp}
+        liked={chat.liked}
+      />
+    ));
+  };
+
   return (
-    <div className='chat-entry local'>
-      <h2 className='entry-name'>Replace with name of sender</h2>
-      <section className='entry-bubble'>
-        <p>Replace with body of ChatEntry</p>
-        <p className='entry-time'>Replace with TimeStamp component</p>
-        <button className='like'>🤍</button>
-      </section>
-    </div>
+    <section className='chat-entry local'>
+      {getChatLogJSX(props.chatEntries)}
+    </section>
+    /* <h2 className='entry-name'>Replace with name of sender</h2>
+    <section className='entry-bubble'>
+      <p>Replace with body of ChatEntry</p>
+      <p className='entry-time'>Replace with TimeStamp component</p>
+      <button className='like'>🤍</button>
+    </section> */
   );
 };
 
