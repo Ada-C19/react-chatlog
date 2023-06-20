af85d4e20765ff2c5dc611ff0e1a05934bb5dc00
Edith <mxedithbaltazar@gmail.com>
2023-06-19 17:00:02 -0700

    added props

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -5,13 +5,18 @@ import TimeStamp from './TimeStamp';
 
 
 
-const ChatEntry = ({sender, body, timeStamp}) => {
+const ChatEntry = (props) => {
+
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name"> { sender }</h2>
+      <h2 className="entry-name"> { props.sender }</h2>
       <section className="entry-bubble">
-        <p>{ body } </p>
-        <p className="entry-time">{ timeStamp } </p>
+        <p>{ props.body } </p>
+        <p className="entry-time"><TimeStamp>
+          { props.timeStamp }
+        </TimeStamp>
+          
+        </p>
         <button className="like">🤍</button>
       </section>
     </div>
