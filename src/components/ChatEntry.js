69c981223f3a449352f53a3321a11692224078b7
fabiola gonzalez <gonzalezfabs@gmail.com>
2023-06-20 14:26:43 -0700

    passes waves 1+ 2, header updated, chat messages split

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -12,10 +12,11 @@ const ChatEntry = (props) => {
     setLiked(!liked);
   };
 
+  const chatEntryClass = props.sender === 'Estragon' ? 'chat-entry remote' : 'chat-entry local';
   
   
   return (
-    <div className="chat-entry local">
+    <div className={chatEntryClass}>
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
