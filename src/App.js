3e1c04eadcc1c5705d2436594823b1cbb3716edc
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-06-20 10:09:45 -0400

    complete wave 1 and pass wave 1 tests

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const firstChat = chatMessages[0]
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        <ChatEntry sender={firstChat.sender} body={firstChat.body} timeStamp={firstChat.timeStamp}></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
