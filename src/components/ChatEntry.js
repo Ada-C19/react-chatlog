ba05a90ad2b5cb28c043e6955a368950c78a0b1b
Aiyana Ball <ballaiyana@gmail.com>
2023-06-20 19:10:50 -0700

    Created handleClick function to change the color of the heart on click

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -7,13 +7,19 @@ import TimeStamp from './TimeStamp.js';
 const ChatEntry = (props) => {
   const localSender = props.sender === 'Vladimir';
 
+  const handleClick = () => {
+    props.onLike();
+  }
+
   return (
     <div className={`chat-entry ${localSender ? 'local' : 'remote'}`}>
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
         <p className="entry-time"><TimeStamp time = {props.timeStamp}/></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={handleClick}>
+          {props.liked ? '❤️' : '🤍'}
+        </button>
       </section>
     </div>
   );
@@ -23,6 +29,7 @@ ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
 };
 
 export default ChatEntry;
