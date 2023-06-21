15bd6fd5dcc0dc4331a56e4e3ff71e53e672bf1f
Aiyana Ball <ballaiyana@gmail.com>
2023-06-20 19:10:11 -0700

    Created toggleLike function to keep track of liked messages; reformatted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 
 const App = () => {
+  const [likedMessages, setLikedMessages] = useState(new Set());
+
+  const toggleLike = (id) => {
+    const newLikedMessages = new Set(likedMessages);
+      if (likedMessages.has(id)) {
+        newLikedMessages.delete(id);
+      } else {
+        newLikedMessages.add(id);
+      }
+      setLikedMessages(newLikedMessages);
+    };
+
   return (
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
+        <h2>{likedMessages.size} ❤️s</h2>
       </header>
       <main>
-        <ChatLog entries={chatMessages} />
+        <ChatLog 
+          entries={chatMessages}
+          onLike={toggleLike}
+          likedMessages={likedMessages}
+          />
       </main>
     </div>
   );
