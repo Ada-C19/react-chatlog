a4d4625a1c40842dab3ca2ef3747a3f852941e9c
an2084 <angelanawang47@gmail.com>
2023-06-20 22:01:50 -0400

    Created an instance of one ChatEntry

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -9,8 +10,16 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <div className="contacts">
+          <ChatEntry 
+              id="1"
+              sender="Vladimir"
+              body="why are you arguing with me"
+              timeStamp="2018-05-29T22:49:06+00:00"
+              liked= "false"
+          />
+        </div>
+      {/* <ChatEntry chatMessages={chatMessages}/> */}
       </main>
     </div>
   );
