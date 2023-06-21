6d9b5ffc842c321366b5ae6396a9888d9964c418
an2084 <angelanawang47@gmail.com>
2023-06-20 23:52:08 -0400

    Created a conditional for Estragon to move messages to right of page

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -7,10 +7,13 @@ const ChatEntry = (props) => {
   // console.log(chatMessages)
   // const senderPerson = props.sender.map()
   // const{sender, body, timestamp} = props;
-  
+
+
+  if (props.sender === 'Vladimir') {
     return (
       <div className="chat-entry local">
-        <h2 className="entry-name">{props.sender}</h2>
+        {/* {props.name === 'Estragon && <div className="Estragon-right"></div>} */}
+        {props.sender === 'Vladimir' && <h2 className="entry-name">{props.sender}</h2>}
         <section className="entry-bubble">
           <p>
             {props.body}
@@ -19,8 +22,22 @@ const ChatEntry = (props) => {
           <button className="like">🤍</button>
         </section>
       </div>
-    );
+    )
+  } else {
+    return (
+      <div className="chat-entry remote">
+        {props.sender === 'Estragon' && <h2 className="entry-name">{props.sender}</h2>}
+        <section className="entry-bubble">
+          <p>
+            {props.body}
+          </p>
+          <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
+          <button className="like">🤍</button>
+        </section>
+      </div>
+  )};
 };
+
 
 ChatEntry.propTypes = {
   id: PropTypes.number.isRequired,
