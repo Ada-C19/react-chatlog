49208d347619dd2dec375bff266b953a702f04d6
Gabby <gabrielleyoung10@gmail.com>
2023-06-20 21:01:47 -0500

    Complete Wave 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry'
+import messages from './data/messages.json'
 
 const App = () => {
+  const chatEntries = messages.map(message => {
+    return <ChatEntry 
+    sender={message.sender} 
+    body={message.body} 
+    timeStamp={message.timeStamp} 
+    />
+  });
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
+        {chatEntries}
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
