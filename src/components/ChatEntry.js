8df6c023eb6d61aa27ed699bb0fcad3879bd769e
Stacy <stacydodson5@gmail.com>
2023-06-21 13:13:31 -0400

    Finished line that switches emojis

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = ({id, sender, body, timeStamp, liked, setLikes }) => {
-  const buttonClass = liked ? ''
+  const buttonClass = liked ?  '❤️':'🤍'
   return (
     <div className="chat-entry local">
       <h2 className="entry-name"> { sender } </h2>
       <section className="entry-bubble">
         <p> { body } </p>
         <TimeStamp time = {timeStamp} className="entry-time"></TimeStamp>
-        <button className="like" onClick = { () => setLikes(id) } >🤍</button>
+        <button className="like" onClick = { () => setLikes(id) } > {buttonClass}
+        </button>
       </section>
     </div>
   );
