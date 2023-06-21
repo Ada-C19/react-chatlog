5c2c5bd356144469914517a45741506dc5c9c95e
Jasmin Worthy <jasminworthy@gmail.com>
2023-06-21 14:13:54 -0500

    update chatentry and app for wave1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const firstChatMessage = chatMessages[0];
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={firstChatMessage.sender}
+          body={firstChatMessage.body}
+          timeStamp={firstChatMessage.timeStamp}
+        />
       </main>
     </div>
   );
