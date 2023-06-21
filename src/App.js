7f4ca7fa2bd62d022260d21d037df3a0436723a4
Julie Loh <jlohusky@gmail.com>
2023-06-20 19:19:00 -0700

    refactor countLikedChatEntries

--- src/App.js
+++ src/App.js
@@ -6,26 +6,6 @@ import ChatLog from './components/ChatLog';
 
 const App = () => {
   const [chatEntries, setChatEntries] = useState(chatMessages);
-  // const chatData = [
-  //   {
-  //     id: 1,
-  //     sender:'Vladimir',
-  //     body:'why are you arguing with me',
-  //     timeStamp:'2018-05-29T22:49:06+00:00',
-  //     liked: false,
-  //   }
-  // ];
-
-  // const entries = data;
-
-  // const chatEntryComponents = entries.map(chatMsg => {
-  //   return (
-  //     <ul>
-  //       <ChatEntry name={chatMsg.sender} body={chatMsg.body} timeStamp={chatMsg.timeStamp}></ChatEntry>
-  //     </ul>
-
-  //   )
-  // });
 
   const updateChatEntries = updatedChatEntry => {
     const chatMsgs = chatEntries.map(chatEntry => {
@@ -37,19 +17,15 @@ const App = () => {
     })
 
     setChatEntries(chatMsgs);
-    // console.log(likedChatEntries().length);
   };
 
-  const likedChatEntries = () => {
-    return (chatEntries.map(chatEntry => chatEntry.liked).filter(item => item === true))};
-
-  const countLikes = likedChatEntries().length;
+  const countLikedChatEntries = chatEntries.filter(chatEntry => chatEntry.liked === true).length;
 
   return (
     <div id="App">
       <header>
         <h1>Chatlog</h1>
-        <section>{countLikes} ❤️s</section>
+        <section>{countLikedChatEntries} ❤️s</section>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
