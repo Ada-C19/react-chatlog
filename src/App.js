b21bab4f85c78a27a5c2c859e4507253759c2b64
Sarah Kim <kimsuyon2013@gmail.com>
2023-06-20 22:26:22 -0700

    lifted state up, created a function for it then counted the total hearts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import { useState } from 'react';
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog'
 
 
 const App = () => {
+  // updating state for each chat Entry 
+  const [chatEntryData, setLikeStatus] = useState(chatMessages);
+  const updateChatData = (updatedChat) => {
+    const chats = chatEntryData.map((chat) => {
+      if (chat.id === updatedChat.id){
+        return updatedChat; 
+      } else {
+        return chat;
+      }
+    });
+    setLikeStatus(chats);
+  };
+
+  let totalBoos = 0;
+  for (let entry of chatEntryData){
+    if (entry.liked === true){
+      totalBoos++;
+    };
+  };
   return (
     <div id="App">
       <header>
         <h1> Vladmir vs. Estragon</h1>
+        <section>
+          <h2 id="heartWidget" className="widget">{totalBoos}👎s</h2>
+        </section>
       </header>
       <main>
-        <ChatLog entries={chatMessages}/>
+        <ChatLog 
+          entries={chatEntryData} 
+          onChatState={updateChatData} 
+        />
       </main>
     </div>
   );
-};
+}
 
 export default App;
