d17b2549dddeed5e2948c8891875dfaaf0395ae1
Jasmin Worthy <jasminworthy@gmail.com>
2023-06-21 15:11:22 -0500

    added chatlog component and modified app component to utilize chatlog and pass 7 tests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
-  const firstChatMessage = chatMessages[0];
-
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        <ChatEntry
-          sender={firstChatMessage.sender}
-          body={firstChatMessage.body}
-          timeStamp={firstChatMessage.timeStamp}
-        />
+        <ChatLog entries={chatMessages} />
       </main>
     </div>
   );
