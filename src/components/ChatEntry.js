1388d73aa21c938c83aee9c4db3c70c9377cc84d
Yael Peet <yaelpeet@gmail.com>
2023-06-20 21:03:30 -0700

    timestamp in chat entry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = ({id, sender, body, timeStamp, liked}) => {
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
-        <p className="entry-time">{timeStamp}</p>
-        <button className="like">{liked}🤍</button>
+        <p className="entry-time">    
+        <TimeStamp time ={timeStamp}/>
+        </p>
+        <button> 🤍 </button>
       </section>
     </div>
   );
@@ -20,7 +24,7 @@ ChatEntry.propTypes = {
   body: PropTypes.string.isRequired,
   sender: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
-  liked: PropTypes.bool.isRequired,
+  liked: PropTypes.bool,
 };
 
 export default ChatEntry;
