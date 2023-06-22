1e70d24222df48194419f4942742870d96ac7926
Carline <carlinesenat@gmail.com>
2023-06-21 23:22:02 -0700

    Completed Wave 1 - created props required and fixed timestamp to display!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
+import TimeStamp from './components/TimeStamp';
+
 
 const App = () => {
   console.log(chatMessages);
@@ -10,6 +13,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
+      <ChatEntry></ChatEntry>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
