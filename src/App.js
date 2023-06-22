09f4c9ac7ed378cf0414566b8e42fc7bdc85e3f4
Barbara <barbaraelisabethb@gmail.com>
2023-06-22 11:49:37 -0400

    Completed Wave 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
-import TimeStamp from './components/TimeStamp';
+import ChatLog from './components/ChatLog';
+
+// import ChatEntry from './components/ChatEntry';
+// import TimeStamp from './components/TimeStamp';
 
 const App = () => {
+  const [messages, setMessages] = useState(chatMessages);
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        <ChatEntry 
+
+        {/*         <ChatEntry 
         sender={chatMessages[0]['sender']} 
         body={chatMessages[0]['body']} 
         timeStamp={chatMessages[0]['timeStamp']} 
-        />
+        /> */}
+        
+        <ChatLog entries={messages} />
       </main>
     </div>
   );
