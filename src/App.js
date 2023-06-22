557c37dde993a7556ff921ae3ca3cddcb994976b
Raissa Barbosa <raissaglaucie@icloud.com>
2023-06-22 12:52:11 -0400

    import chatEntry and TimeStamp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
-  return (
-    <div id="App">
-      <header>
-        <h1>Application title</h1>
-      </header>
-      <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
-      </main>
-    </div>
-  );
+	console.log(chatMessages);
+	return (
+		<div id='App'>
+			<header>
+				<h1>Chat Log</h1>
+			</header>
+			<main>
+				<ChatEntry
+					sender={chatMessages[0].sender}
+					body={chatMessages[0].body}
+					timeStamp={chatMessages[0].timeStamp}
+				/>
+			</main>
+		</div>
+	);
 };
 
 export default App;
