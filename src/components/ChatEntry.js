e3e42662fee90c1210e2248be18a17d09094455b
Jasmin Worthy <jasminworthy@gmail.com>
2023-06-22 21:23:41 -0500

    wave3 tests pass and update all components

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
-import TimeStamp from './TimeStamp';
 
-const ChatEntry = (props) => {
-  const { sender, body, timeStamp } = props;
+const ChatEntry = ({ sender, body, timeStamp, id, liked, handleLike }) => {
+  const getTimeAgo = () => {
+    const now = new Date();
+    const messageTime = new Date(timeStamp);
+    const timeDifference = now - messageTime;
+    const diffYears = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
+
+    return `${diffYears} years ago`;
+  };
 
   return (
-    <div className="chat-entry local">
+    <div className={`chat-entry ${liked ? 'local liked' : 'local'}`}>
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
-        <p className="entry-time">
-          <TimeStamp time={timeStamp} />
-        </p>
-        <button className="like">🤍</button>
+        <p className="entry-time">{getTimeAgo()}</p>
+        <button className="like" onClick={() => handleLike(id)}>
+          {liked ? '❤️' : '🤍'}
+        </button>
       </section>
     </div>
   );
@@ -24,7 +30,9 @@ ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  liked: PropTypes.bool.isRequired,
+  handleLike: PropTypes.func.isRequired,
 };
 
 export default ChatEntry;
-
