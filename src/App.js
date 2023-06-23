5316764bc34639961c7f1aec9283974d0a8155fc
Carline <carlinesenat@gmail.com>
2023-06-23 01:40:24 -0700

    Passes all waves

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
-import TimeStamp from './components/TimeStamp';
 import ChatLog from './components/ChatLog';
 
 
 const App = () => {
-  console.log(chatMessages);
+  const [messages, setMessages] = useState(chatMessages);
+  
+  const updateLikes = (id) => {
+    setMessages((prev) => {
+      return prev.map((entry) => {
+        if (id === entry.id) {
+          return {
+            ...entry,
+            liked: !entry.liked};
+        } else {
+          return entry;
+        };
+      });
+    });
+  };
+
+  const totalLikesCount = messages.reduce((total, entry) => {
+    if (entry.liked === true) {
+      total += 1;
+    }
+    return total;
+  },0);
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Magic Messenger</h1>
+        <h2>Chat between you and Vladmir</h2>
+        <div id='heartWidget' className='widget'>{totalLikesCount} ❤️s</div>
       </header>
       <main>
-      <ChatEntry></ChatEntry>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
-      <ChatLog entries={chatMessages}/>
+      <ChatLog entries={chatMessages} onUpdateLikes={updateLikes}/>
       </main>
     </div>
   );
--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+  // const clickButton = () => {
+  //   props.updateLikes(props.id);
+  // };
+  // const likeButton = props.liked ? '❤️' : '🤍 ';
+
+
   return (
-    <div className="chat-entry local">
+    <div className={props.sender === 'Estragon' ? 'chat-entry remote' : 'chat-entry local'}>
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
         <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={() => props.onUpdateLikes(props.id)}>{props.liked ? '❤️' : '🤍 '}</button>
       </section>
     </div>
   );
@@ -21,6 +27,10 @@ ChatEntry.propTypes = {
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  id: PropTypes.number.isRequired,
+  onUpdateLikes: PropTypes.func.isRequired
+
 
 };
 
