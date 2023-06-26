270651de3471d8f3cb8eea8029ac38e2baf2e40a
Anh Huynh <avh2@uw.edu>
2023-06-26 02:28:20 -0700

    Modified App.js and ChatEntry.js files and passed Wave 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const chatData = 
+    {
+      'sender':'Vladimir',
+      'body':'why are you arguing with me',
+      'timeStamp':'2018-05-29T22:49:06+00:00',
+    }
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry sender={chatData.sender} body={chatData.body} timeStamp={chatData.timeStamp}></ChatEntry>
+        {/* Wave 02: Render ChatLog component */}
       </main>
     </div>
   );
