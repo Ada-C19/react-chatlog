81b070aa6082b34f6de497e32e193b3e9cc7d13f
abbymachines <abby.machines@gmail.com>
2023-06-28 13:53:37 -0700

    clean up App component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import chatMessages from './data/messages.json';
 import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 import messages from './data/messages.json'
-// import './components/ChatLog.css';
-
-// const testMessage = {
-//   id: 1,
-//   sender:'Vladimir',
-//   body:'why are you arguing with me',
-//   timeStamp:'2018-05-29T22:49:06+00:00',
-//   liked: false
-// };
-
-// const messageComponents = {
-  
-// }
 
 const App = () => {
   const [chatMessages, setChatMessages] = useState(chatMessages)
@@ -42,10 +29,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
         <ChatLog entries={messages} onLikeMessage={likeMessage} />
-        {/* <ChatEntry sender={testMessage.sender} body={testMessage.body} timeStamp={testMessage.timeStamp} /> */}
       </main>
     </div>
   );
