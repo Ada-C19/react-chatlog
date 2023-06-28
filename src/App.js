217c191105165fb409f94bd8f87ca4dcf5e4a6a8
Abby Castillo <abby.machines@gmail.com>
2023-06-27 23:05:42 -0700

    add testMessage variable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 
+const testMessage = {
+  "id": 1,
+  "sender":"Vladimir",
+  "body":"why are you arguing with me",
+  "timeStamp":"2018-05-29T22:49:06+00:00",
+  "liked": false
+};
+
 const App = () => {
   return (
     <div id="App">
