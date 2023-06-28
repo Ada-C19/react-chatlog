6b24159e0abe4deb21ed7a7ce2f26bc1eea7a77e
Allie Soliz <solizale@gmail.com>
2023-06-28 15:30:09 -0400

    adds heart counter and updated entries

--- src/App.js
+++ src/App.js
@@ -2,19 +2,49 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
+import { useState } from 'react';
 
 const App = () => {
+	// const [likesCount, setLikesCount] = useState(0);
+	const [entries, setEntries] = useState(chatMessages);
+
+	const sumHearts = () => {
+		let total = 0;
+
+		entries.forEach((entry) => {
+			if (entry.liked) {
+				total++;
+			}
+		});
+		return total;
+	};
+
+	const switchHeart = (updatedEntry) => {
+		const updatedEntries = entries.map((entry) => {
+			if (entry.id === updatedEntry.id) {
+				// entry.liked = !entry.liked;
+
+				return { ...entry, liked: !entry.liked };
+			}
+			return entry;
+		});
+		setEntries(updatedEntries);
+	};
+
 	return (
 		<div id="App">
 			<header>
 				<h1>Allie's Chat App</h1>
+				<section className="black" id="heartWidget">
+					{sumHearts()} ❤️s
+				</section>
 			</header>
 
 			<main>
 				{/* Wave 01: Render one ChatEntry component */}
 
 				{/* Wave 02: Render ChatLog component */}
-				<ChatLog entries={chatMessages}></ChatLog>
+				<ChatLog entries={entries} onUpdateHeart={switchHeart}></ChatLog>
 			</main>
 		</div>
 	);
