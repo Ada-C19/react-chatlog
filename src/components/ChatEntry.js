9b4c2efa378cd80782ba9e8ebbc72540685d7dbd
Abby Castillo <abby.machines@gmail.com>
2023-06-27 23:35:03 -0700

    format props in ChatEntry

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -9,13 +9,13 @@ const ChatEntry = ({messageData}) => {
 
   return (
     <div className="chat-entry local">
-      {/* <h2 className="entry-name">{props.sender}</h2>
+      <h2 className="entry-name">{message.sender}</h2>
       <section className="entry-bubble">
-        <p>{props.body}</p>
-        <p className="entry-time"><TimeStamp /></p>
+        <p>{message.body}</p>
+        <p className="entry-time"><TimeStamp time={message.timeStamp}/></p>
         <button className="like">🤍</button>
-      </section> */}
-      <p>sender: {message.sender}</p>
+      </section>
+      {/* <p>sender: {message.sender}</p> */}
     </div>
   );
 };
