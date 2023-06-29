77ac454643e2fabf9e23d3d16ac92e5eb2843097
abbymachines <abby.machines@gmail.com>
2023-06-29 02:37:57 -0700

    continue building out toggleLiked in App component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import './App.css';
 // import chatMessages from './data/messages.json';
 // import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
-import messages from './data/messages.json'
+import messagesJSON from './data/messages.json'
 
 const App = () => {
-  const [messageData, setMessageData] = useState(messages);
+  const [messageData, setMessageData] = useState(messagesJSON);
 
-  const updateMessageData = updatedMessage => {
-    console.log('shout out to updateMessageData');
-    const tempMessages = messageData.map(message => {
-      if (message.id === updatedMessage.id) {
-        return updatedMessage;
-      } else {
-        return message;
+  // const updateMessageData = updatedMessage => {
+  //   console.log('shout out to updateMessageData');
+  //   const tempMessages = messageData.map(message => {
+  //     if (message.id === updatedMessage.id) {
+  //       return updatedMessage;
+  //     } else {
+  //       return message;
+  //     }
+  //   });
+  const toggleLiked = (id) => {
+    const messages = messageData.map((message) => {
+      if (message.id === id) {
+        message.liked = !message.liked;
       }
+
+      return message;
     });
 
-    setMessageData(tempMessages)
-  };
+    setMessageData(messages);
+  }
+
+
+  //   setMessageData(tempMessages)
+  // };
 
   return (
     
@@ -28,7 +40,7 @@ const App = () => {
         <h1>Application title</h1>
       </header>
       <main>
-        <ChatLog entries={messages} onLikeMessage={updateMessageData}/>
+        <ChatLog entries={messagesJSON} onLikeMessage={toggleLiked}/>
       </main>
     </div>
   );
