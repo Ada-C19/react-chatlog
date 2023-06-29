7303bbbe831a970522078a19f98efaf8c0719475
abbymachines <abby.machines@gmail.com>
2023-06-29 03:25:36 -0700

    remove extraneous comments from app component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, {useState} from 'react';
 import './App.css';
-// import chatMessages from './data/messages.json';
-// import ChatEntry from './components/ChatEntry';
 import ChatLog from './components/ChatLog';
 import messagesJSON from './data/messages.json'
 
 const App = () => {
   const [messageData, setMessageData] = useState(messagesJSON);
 
-  // const updateMessageData = updatedMessage => {
-  //   console.log('shout out to updateMessageData');
-  //   const tempMessages = messageData.map(message => {
-  //     if (message.id === updatedMessage.id) {
-  //       return updatedMessage;
-  //     } else {
-  //       return message;
-  //     }
-  //   });
   const toggleLiked = (id) => {
     console.log('im toggling likes ova heare');
     const messages = messageData.map((message) => {
@@ -29,10 +18,6 @@ const App = () => {
 
     setMessageData(messages);
   }
-
-
-  //   setMessageData(tempMessages)
-  // };
 
   return (
     
