dc42f7e1f420e245b030a2d1520272b58ccd5244
abbymachines <abby.machines@gmail.com>
2023-06-29 16:51:30 -0700

    add button and onClick to HeartCounter component

--- src/components/HeartCounter.js
+++ src/components/HeartCounter.js
@@ -1,12 +1,17 @@
 import '../App.css';
 
 const HeartCounter = () => {
+  const printMessage = () => {
+    console.log('Hello?? can\'t a program count hearts in peace??');
+  };
+
   return (
     <div>
       <br></br>
       <p className='heartWidget'>
         shout out to a certain amount of likes clicked
       </p>
+      <button onClick={printMessage}>hello!</button>
     </div>
   );
 };
