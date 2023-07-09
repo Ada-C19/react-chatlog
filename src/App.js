ae3fbf32293a98c3add440f159ca46d70501d27f
Angie Tran <tranangiekim@gmail.com>
2023-07-09 00:20:02 -0600

    set-up state handling for like button in App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
+import { useState } from 'react';
 
 const App = () => {
+  const [chatDATA, setchatDATA] = useState(chatMessages);
+  const updateChat = (updatedEntry) => {
+    setchatDATA((prev) => {
+      return prev.map((entry) => {
+        if (entry.id === updatedEntry.id) {
+          return updatedEntry;
+        } else {
+          return entry;
+        }
+      });
+    });
+  };
+
   return (
     <div id="App">
       <header>
         <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        <ChatLog entries={chatMessages}/>
+        <ChatLog entries={chatDATA} onLikeMessage={updateChat}/>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
