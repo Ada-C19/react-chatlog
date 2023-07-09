e5713dd6f47e8f4440ee632aa74016560d1f1e17
Angie Tran <tranangiekim@gmail.com>
2023-07-08 22:42:12 -0600

    changed rendering of ChatEntry to ChatLog in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
-import ChatEntry from './components/ChatEntry';
+import ChatLog from './components/ChatLog';
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
         <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        <ChatEntry entries={chatMessages}/>
+        <ChatLog entries={chatMessages}/>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
