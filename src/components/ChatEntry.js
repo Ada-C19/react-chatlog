0fda4da2fc1ff031d1becd7bcdc728b0c7e5c8d1
Angie Tran <tranangiekim@gmail.com>
2023-07-09 00:30:55 -0600

    set-up event handling for like button in ChatEntry.js

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,13 +4,27 @@ import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp.js';
 
 const ChatEntry = (props) => {
+
+  const onLikeBtnClick = () => {
+    const updatedEntry = {
+      id: props.id,
+      sender: props.sender,
+      body: props.body,
+      timeStamp: props.timeStamp,
+      liked: !props.liked
+    }
+    props.onUpdate(updatedEntry)
+  }
+  
+  const heartStatus = props.liked ? '❤️' : '🤍';
+
   return (
     <div className="chat-entry local">
       <h2 className="entry-name">{props.sender}</h2>
       <section className="entry-bubble">
         <p>{props.body}</p>
         <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={onLikeBtnClick}>{heartStatus}</button>
       </section>
     </div>
   );
