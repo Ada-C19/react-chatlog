6b514a1ad6a36af0377965a38a99dfd6fae9141f
alyssa <tn.alyssa@gmail.com>
2023-07-14 16:28:04 -0500

    rendered an example of a chat entry

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+
+  const exampleMessage = chatMessages[0]
+
   return (
     <div id="App">
       <header>
         <h1>Application title</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        {
+        /* Wave 01: Render one ChatEntry component
+        Wave 02: Render ChatLog component */
+          <ChatEntry body={exampleMessage.body} timeStamp={exampleMessage.timeStamp} sender={exampleMessage.sender}/>
+        }
       </main>
     </div>
   );
