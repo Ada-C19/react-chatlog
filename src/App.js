df081d1b417888bfaffc545007084759b90fc356
Amber Shay <mashaad21@gmail.com>
2023-07-31 17:00:47 -0600

    wave 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import TimeStamp from './components/TimeStamp';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
@@ -11,9 +13,11 @@ const App = () => {
       <main>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
+        <ChatEntry sender={chatMessages[0].sender} body={chatMessages[0].body} timeStamp={chatMessages[0].timeStamp} />
       </main>
     </div>
   );
 };
+
 
 export default App;
